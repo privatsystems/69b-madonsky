30972082cf6ecc672ed5f7e250286952b54fda50
gitMarPriv <git@privat.systems>
2025-06-19 09:21:03 +0200

    ss

--- front/src/components/vimeoPlayer.tsx
+++ front/src/components/vimeoPlayer.tsx
@@ -10,13 +10,14 @@ type Props = {
 const horloge = '<svg viewBox="0 0 73 73" width="18" height="18"><path d="M36.5,73C16.37,73,0,56.63,0,36.5S16.37,0,36.5,0s36.5,16.37,36.5,36.5-16.37,36.5-36.5,36.5ZM36.5,8c-15.71,0-28.5,12.79-28.5,28.5s12.79,28.5,28.5,28.5,28.5-12.79,28.5-28.5-12.79-28.5-28.5-28.5Z" /><polygon points="32.78 16.03 32.78 38.11 24.55 46.33 29.86 51.64 40.46 41.03 40.46 16.03 32.78 16.03" /></svg>'
 
 const VimeoPlayer = ({ videoId, videoLegend }: Props) => {
+
     const containerRef = useRef<HTMLDivElement>(null);
     const playerRef = useRef<Player | null>(null);
     const [ratio, setRatio] = useState(16 / 9);
     const [height, setHeight] = useState(600);
     const [width, setWidth] = useState(1066);
 
-    const { isMob } = useContext(SiteContext)
+    const { isMob } = useContext(SiteContext);
 
     useEffect(() => {
         if (!containerRef.current || !videoId) return;
@@ -53,6 +54,7 @@ const VimeoPlayer = ({ videoId, videoLegend }: Props) => {
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
+            console.log('isMob', isMob);
             if (!isMob) {
                 setHeight(window.innerHeight * 0.6);
                 setWidth(window.innerHeight * 0.6 * ratio);
